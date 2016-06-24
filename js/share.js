3cff117c76778d2767b20b69fed8355cd21a7cf9
wangjiankui <952035865@qq.com>
2016-06-24 14:47:27 +0800

    m

--- js/share.js
+++ js/share.js
@@ -1,33 +1,33 @@
+var imgUrl = "http://minisite.hocodo.com/test/recruitMain/images/share.jpg";  //注意必须是绝对路径
 
-var imgUrl = 'http://topic.xcar.com.cn/201403/ad_q3/pic/banner.jpg';
-var lineLink = '【奥迪Q3开启尊享礼遇季】报名试驾，赢取精美礼品';
-var descContent = "【奥迪Q3开启尊享礼遇季】报名试驾，赢取精美礼品";
-var shareTitle = '【奥迪Q3开启尊享礼遇季】报名试驾，赢取精美礼品';
-var appid = 'wxc9937e3a66af6dc8';
+var lineLink = "https://manyi-project.github.io/propaganda/";   //同样，必须是绝对路径
+var descContent = '最轻便的微电台精选，天天更新，你听或是不听，我都这里等着你。'; //分享给朋友或朋友圈时的文字简介
+var shareTitle = '微信电台精选';  //分享title
+var appid = ''; //apiID，可留空
 
 function shareFriend() {
     WeixinJSBridge.invoke('sendAppMessage',{
         "appid": appid,
         "img_url": imgUrl,
-        "img_width": "640",
-        "img_height": "640",
+        "img_width": "200",
+        "img_height": "200",
         "link": lineLink,
         "desc": descContent,
         "title": shareTitle
     }, function(res) {
-        _report('send_msg', res.err_msg);
+        //_report('send_msg', res.err_msg);
     })
 }
 function shareTimeline() {
     WeixinJSBridge.invoke('shareTimeline',{
         "img_url": imgUrl,
-        "img_width": "640",
-        "img_height": "640",
+        "img_width": "200",
+        "img_height": "200",
         "link": lineLink,
         "desc": descContent,
         "title": shareTitle
     }, function(res) {
-        _report('timeline', res.err_msg);
+        //_report('timeline', res.err_msg);
     });
 }
 function shareWeibo() {
@@ -35,24 +35,21 @@ function shareWeibo() {
         "content": descContent,
         "url": lineLink,
     }, function(res) {
-        _report('weibo', res.err_msg);
+        //_report('weibo', res.err_msg);
     });
 }
-// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。 
+// 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
 document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-
-// 发送给好友 
+    // 发送给好友
     WeixinJSBridge.on('menu:share:appmessage', function(argv){
         shareFriend();
     });
-
-// 分享到朋友圈 
+    // 分享到朋友圈
     WeixinJSBridge.on('menu:share:timeline', function(argv){
         shareTimeline();
     });
-
-// 分享到微博 
+    // 分享到微博
     WeixinJSBridge.on('menu:share:weibo', function(argv){
         shareWeibo();
     });
-}, false); 
+}, false);
